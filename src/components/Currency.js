d432cec947573e930b8dbc4b0aa49bf1ea0ba269
Vikram S <vikram.samant@gmail.com>
2023-02-05 18:07:15 -0500

    Fixed currency layout, need to update styling though

--- src/components/Currency.js
+++ src/components/Currency.js
@@ -5,57 +5,50 @@ import { AppContext } from '../context/AppContext';
 const Currency = () => {
     const { currency, dispatch  } = useContext(AppContext);
     const [lcurrency, setLcurrency] = useState(currency);
-    const [lname, setLname] = useState('Pound');
+    const [showOptions, setShowOptions] = useState(false);
 
     const submitEvent = (newc) => {
         console.log("Params "  + newc);
         setLcurrency(newc);
-        let newn = lname;
-        switch (newc) {
-            case '$':
-                newn = 'Dollar';break;
-            case '£':
-                newn = 'Pound';break;
-            case '€':
-                newn = 'Euro';break;
-            case '₹':
-                newn = 'Ruppee';break;
-            default:
-                newn = 'Unknown';
 
-        }
-        setLname(newn);
         dispatch({
             type: 'CHG_CURRENCY',
             payload: newc,
         });
+        toggleCurrencyOptions();
     }
-/*
-    return (
-        <div className='alert alert-primary'>
-            <span>
-                Currency ({lcurrency} {lname})
-            <select className="custom-select" id="inputGroupSelect01" onChange={(event) => submitEvent(event.target.value)}>
-                <option value="$" name="Dollar">$ Dollar</option>
-                <option value="£" name="Pound">£ Pound</option>
-                <option value="€" name="Euro">€ Euro</option>
-                <option value="₹" name="Rupee">₹ Ruppee</option>
-            </select>
-            </span>
 
-        </div>
-    );
-    */
-   return (
-    <div className='dropdown'>
-        <button>Currency ({lcurrency} {lname})</button>
-        <div class="dropdown-options">
-            <a href="#" onClick={()=>submitEvent('$')}>$ Dollar</a>
-            <a href="#" onClick={()=>submitEvent('£')}>£ Pound</a>
-            <a href="#" onClick={()=>submitEvent('€')}>€ Euro</a>
-            <a href="#" onClick={()=>submitEvent('₹')}>₹ Ruppee</a>
-        </div>
-    </div>
+    const getCurrencyLabel = (curr) => {
+        switch (curr) {
+            case '$':
+                return '$ Dollar';
+            case '£':
+                return '£ Pound';
+            case '€':
+                return '€ Euro';
+            case '₹':
+                return '₹ Rupee';
+            default:
+                return curr + ' Unknown';
+
+        }
+
+    }
+
+    const toggleCurrencyOptions = () => {
+        setShowOptions(!showOptions); 
+    }
+
+    return (
+   <div className='dropdown'>
+        <button  type="button" className='btn btn-primary dropdown-toggle' onClick={toggleCurrencyOptions}>Currency ({getCurrencyLabel(lcurrency)})</button>
+        <ul className={`custom-menu dropdown-menu ${showOptions ? 'show' : ''} `}>
+            <li><button className="dropdown-item" type="button" onClick={()=>submitEvent('$')}>{getCurrencyLabel('$')}</button></li>
+            <li><button className="dropdown-item" type="button" onClick={()=>submitEvent('£')}>{getCurrencyLabel('£')}</button></li>
+            <li><button className="dropdown-item" type="button" onClick={()=>submitEvent('€')}>{getCurrencyLabel('€')}</button></li>
+            <li><button className="dropdown-item" type="button" onClick={()=>submitEvent('₹')}>{getCurrencyLabel('₹')}</button></li>
+        </ul>
+   </div>
 );   
 };
 
